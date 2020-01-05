29ca1d89cec3a75b59bc35e11e6b0c9417c61f57
DevMule <35449804+DevMule@users.noreply.github.com>
2020-01-05 21:12:11 +0500

    Update tick_tack_toe.py

--- tick_tack_toe.py
+++ tick_tack_toe.py
@@ -4,5 +4,8 @@ from bots_and_uis.ai_bot import AIBot
 from bots_and_uis.random_bot import RandomBot
 from referee import Referee
 
-referee = Referee(ConsoleUI(), RandomBot())
-
+referee = Referee(
+    ConsoleUI(),  # X - player
+    ConsoleUI(),  # 0 - player
+    Desk(3, 3, 3),
+)
