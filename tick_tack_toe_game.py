ffc1a05ee7cd5e25aba0809b1a82412d8b50f1ad
DevMule <35449804+DevMule@users.noreply.github.com>
2020-01-17 13:32:44 +0500

    refactor

---
+++ tick_tack_toe_game.py
@@ -0,0 +1,15 @@
+from desk.desk import Desk
+from bots_and_uis.neural_network_bot import NeuralNetworkBot
+from bots_and_uis.console_game import ConsoleUI
+from bots_and_uis.random_bot import RandomBot
+from bots_and_uis.minimax_bot import MiniMaxBot
+from referee import Referee
+
+referee = Referee(
+    MiniMaxBot(),  # X - player
+    ConsoleUI(),  # 0 - player
+    Desk(3, 3, 3),
+)
+for i in range(10):
+    referee.desk.clear()
+    referee.game_loop()
