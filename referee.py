06330c76181144d8ea9fab2fdaaf21e452ff9e10
DevMule <35449804+DevMule@users.noreply.github.com>
2020-01-18 19:00:46 +0500

    заменил player_1 на player_X, т.к. технически суть не меняет, но для меня удобнее

--- referee.py
+++ referee.py
@@ -15,16 +15,16 @@ RefereeConsts = {
 
 
 class Referee:
-    def __init__(self, player_1, player_0, desk=Desk(3, 3, 3)):
-        self.player_0 = player_0  # 0 - player
-        self.player_1 = player_1  # X - player
+    def __init__(self, player_x, player_0, desk=Desk(3, 3, 3)):
+        self.player_0 = player_0
+        self.player_X = player_x
         self.desk = desk
 
         # self.game_loop()
 
     def swap_players(self):
-        tmp_player = self.player_1
-        self.player_1 = self.player_0
+        tmp_player = self.player_X
+        self.player_X = self.player_0
         self.player_0 = tmp_player
 
     def game_loop(self):
@@ -34,10 +34,10 @@ class Referee:
             self.game_loop()
         else:
             self.player_0.game_ended(self.desk, RefereeConsts[0][self.desk.winner], desk_consts[0])
-            self.player_1.game_ended(self.desk, RefereeConsts[1][self.desk.winner], desk_consts[1])
+            self.player_X.game_ended(self.desk, RefereeConsts[1][self.desk.winner], desk_consts[1])
 
     def ask_for_turn(self):
         current_player = self.player_0
         if self.desk.turn == "X":
-            current_player = self.player_1
+            current_player = self.player_X
         return current_player.make_turn(self.desk)
