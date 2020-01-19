075715b7b0a72fa56db77d1201af2252ae0dcaa2
DevMule <35449804+DevMule@users.noreply.github.com>
2020-01-19 19:02:38 +0500

    обучил бота с помощью minimax bot'а, играет за 500 игр лучше чем за 50к сам с собой

--- learn_bot.py
+++ learn_bot.py
@@ -1,4 +1,6 @@
 from desk.desk import Desk
+from bots_and_uis.random_bot import RandomBot
+from bots_and_uis.minimax_bot import MiniMaxBot
 from bots_and_uis.neural_network_bot import NeuralNetworkBot
 from bots_and_uis.console_game import ConsoleUI
 from referee import Referee
@@ -9,29 +11,28 @@ neural_network_bot = NeuralNetworkBot(
     hidden=81,  # 81 скрытых нейровнов
     outputs=9,  # 9 выходных нейровнов
     learning=True,  # бот будет учиться, менять свой опыт
-    new_experience=False,  # бот при инициализации создаст новый опыт
-    learn_rate=.1,  # коэффициент изменения веса для нейронов при одном цикле изучения
+    new_experience=True,  # бот при инициализации создаст новый опыт
+    learn_rate=1,  # коэффициент изменения веса для нейронов при одном цикле изучения
     learn_decrease_coef=.5,  # коэффициент, с которым падает вес изучения для каждого предыдущего
                              #  шага чтобы не переписывать имеющийся позитивный опыт
 )
 
 referee = Referee(
-    neural_network_bot,  # X - player
+    MiniMaxBot(),  # X - player
     neural_network_bot,  # 0 - player
     Desk(3, 3, 3),
 )
 
-for i in range(50000):
+for i in range(500):
     print(i)
     referee.desk.clear()
     referee.game_loop()
+    referee.swap_players()
+neural_network_bot.save_experience()
+print("опыт сохранён")
 
 referee.player_0 = ConsoleUI()
-
 for i in range(5):
     referee.desk.clear()
     referee.game_loop()
     referee.swap_players()
-
-neural_network_bot.save_experience()
-print("опыт сохранён")
